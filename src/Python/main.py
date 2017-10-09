c30a8d0825a3269ab9143cd4f3b8255136182c12
Charley <croberts185@outlook.com>
2017-10-09 17:54:13 +0100

    Fixed empty function call

--- src/Python/main.py
+++ src/Python/main.py
@@ -26,7 +26,7 @@ def add(args):
     if file_type == "--license":    #  can create 3 types of license, with user details added
 
         if file_arg == None:
-            contents = "Copyright (c) {} {} All Rights Reserved."
+            contents = "Copyright (c) {} {} All Rights Reserved.".format(user_name,str(datetime.now().year))
 
         elif file_arg.lower() == "mit":
             with open("../rsrc/MIT.txt") as file:
